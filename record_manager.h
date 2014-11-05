7490f7ce1d676814aca72d32009f051555946afc
Rye Cao <ryecao@gmail.com>
2014-11-06 00:36:05 +0800

    レコードマネージャ

--- record_manager.h
+++ record_manager.h
@@ -1,19 +1,14 @@
-#ifndef RECORD_MANAGER_H
-#define RECORD_MANAGER_H
+#ifndef RECORD_MANAGER_H_
+#define RECORD_MANAGER_H_
 
-#include "table_info.h"
-#include "attribute_type.h"
-<<<<<<< HEAD
-=======
-#include "sql_command.h"
->>>>>>> 9066a870eda922a78cec972b5019057908e63623
 #include <vector>
 #include <string>
 #include <set>
+#include "table_info.h"
+#include "sql_command.h"
 
 class RecordManager{
 public:
-<<<<<<< HEAD
   //@brief: insert into 语句
   //@params: 插入表的信息; 插入的值.
   int InsertRecord(const TableInfo &table, const std::vector<std::string>& values);  
@@ -45,24 +40,6 @@ private:
   static float FetchFloat(const unsigned char *loc);
   static std::string FetchString(const unsigned char *loc,int len);
   std::map <std::string,std::map <int,int> >  blockStatus;
-=======
-	int rmInsertRecord(const std::string &fileName,const std::vector<AttrType> &entry, const TableInfo &datatable);  
-	void rmAddIndex(const std::string dbName,const std::string BTreeName, const TableInfo &datatable, int itemIndex);
-	void rmDeleteWithIndex(const std::string fileName,int offset, const std::vector<WhereClause> &constraint, const TableInfo &datatable); 
-	void rmDeleteWithoutIndex(const std::string fileName, const std::vector<WhereClause> &constraint, const TableInfo &datatable);	
-	std::vector <std::vector <AttrType> > rmSelectWithIndex(const std::string fileName, int offset, const std::vector<WhereClause> &constraint, const TableInfo &datatable); 
-	std::vector <std::vector <AttrType> > rmSelectWithoutIndex(const std::string fileName, const std::vector<WhereClause> &constraint, const TableInfo &datatable);	
-	std::set<int> rmGetAllOffsets(const std::string &fileName);
-	void rmClear(const std::string fileName) ;
-private:
-	void loadBlockStatus(const std::string &fileName);
-	bool FitinTable(const std::vector <AttrType> &entry, const TableInfo &datatable);
-	void entryToBinary(const std::vector <AttrType> &entry, unsigned char * c,const TableInfo &datatable);
-	std::vector <AttrType> binaryToEntry(unsigned char *c,const TableInfo &datatable);
-	bool FitterTest(const std::vector<WhereClause> &constraint, const std::vector <AttrType> &data); 
-	
-
->>>>>>> 9066a870eda922a78cec972b5019057908e63623
 };
 
 #endif
