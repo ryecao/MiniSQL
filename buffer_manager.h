450216f3e2e8182be39e2edcb84cdf661d56e587
sodabeta7 <sodabeta7@gmail.com>
2014-11-08 23:45:37 +0800

    那个誓言实为无效

--- buffer_manager.h
+++ buffer_manager.h
@@ -2,20 +2,32 @@
 #define BUFFER_MANAGER_
 
 #include <string>
-#include "block.h"
+#include <list>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
 
 class BufferManager {    
 public:
+    static const int BufferSize = 100;
     BufferManager();
-
+    ~BufferManager();
     Block AllocateNewBlock(std::string fname);   //allocate a new block in file '$fname'
-    Block GetBlock(std::string fname, int pos);  //return the block in file '${fname}' start at ${pos}
-    void WriteBlock(Block &);               //save the block's data
-    void FreeBlock(Block &);                //delete a block
-    void ClearBlock(const std::string &fname);   //delete all block in file fname
+    Block GetBlock(const std::string &fname,int offset);  //return the block in file '${fname}' start at ${pos}
+    void WriteBlock(const Block &);               //save the block's data
+    void FreeBlock(const Block &);                //delete a block
+    void ClearFile(const std::string &fname);   //delete all block in file fname
 
 private:
     //depende on implementation
+    std::list<Block> pool;
+    FreeBlockManager fm;    
+    Block AddBlock(const std::string &fname,int offset);
+    void Flush();
+    void WriteBack(const Block &b);
 };
-
 #endif
