6964047223204d3b5d29c1234b2b5e7aa219312d
sodabeta7 <sodabeta7@gmail.com>
2014-11-08 23:44:49 +0800

    那个祈愿却是亵渎

--- block.h
+++ block.h
@@ -1,23 +1,20 @@
 #ifndef BLOCK_H_
 #define BLOCK_H_ 
 
-#include <string>
-
-class Block {    
+class Block {
 public:    
-    Block():pos(0){};
-    Block(const Block &); //must be implemented!
-    bool operator=(const Block &); //must be overloaded!
-    template<class T>
-    void append(const T& x); //add x to the end
-    void seek(int); //set the pointer's pos
-    int getpos() const;  //return the current pointer's pos
-
-    bool is_pined; //pin to prevent to be removed.
-    bool is_dirty; //whether the block has been modified
-    int pos; //current pointer's pos
-    unsigned char data[BLOCK_SIZE]; //data
-    std::string fname; //the filename
+    Block():    pos(0)  {}    
+    Block(const std::string _fname,int _offset):
+        fname(_fname),offset(_offset),is_pined(0),is_dirty(0),pos(0) {};    
+    int getpos() const { //return the current pointer's pos
+        return pos;
+    }   
+    int pos;            //the pos in block
+    int offset;         //the offset in file
+    bool is_pined;
+    bool is_dirty;
+    unsigned char data[BLOCK_SIZE];
+    std::string fname;    
 };
 
 #endif
